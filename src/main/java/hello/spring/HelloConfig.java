074ad535333f6aacd89f502dba82f74faa527cb1
NBguys <gudwns520@gamil.com>
2024-07-05 21:03:58 +0900

    내장 톰캣 예제 스링 부트의 원리 예제 추가

---
+++ src/main/java/hello/spring/HelloConfig.java
@@ -0,0 +1,13 @@
+package hello.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration //주석 처리
+public class HelloConfig {
+
+    @Bean
+    public HelloController helloController() {
+        return new HelloController();
+    }
+}
